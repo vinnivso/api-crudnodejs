b99233144410f8fbd37703c0db59d58ca0e95221
vinnivso <vinnivsob@gmail.com>
2022-01-18 11:20:13 -0300

    Feat: TypeORM setup and config

--- src/interfaces/Product.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-  code_bar: string
-  description: string
-  price: number
-  quantity: number
-}
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata"
 import cors from "cors"
 import express from "express"
 import { router } from "./routes"
