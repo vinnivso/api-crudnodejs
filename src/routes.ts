9c503fd8f80b94bc34f49fa19e1695046322ab58
vinnivso <vinnivsob@gmail.com>
2022-01-18 20:51:49 -0300

    Feat: Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express"
 import { CreateCategoryController } from "./controllers/CreateCategoryController"
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController"
 import { GetAllCategoryController } from "./controllers/GetAllCategoryController"
 
 const routes = Router()
 
+/**
+ * C - Create
+ * R - Read
+ * U - Update
+ * D - Delete
+*/
+
+
 routes.post("/categories", new CreateCategoryController().handler)
 routes.get("/categories", new GetAllCategoryController().handler)
+routes.delete("/categories/:id", new DeleteCategoryController().handler)
 
 export {routes}
