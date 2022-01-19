9cce2235d9f4010d94e8118cae13ae8ddaf6f486
vinnivso <vinnivsob@gmail.com>
2022-01-18 21:14:51 -0300

    Update: routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { CreateCategoryController } from "./controllers/CreateCategoryController"
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController"
 import { GetAllCategoryController } from "./controllers/GetAllCategoryController"
+import { UpdateCategoryController } from "./controllers/UpdateCategoryController"
 
 const routes = Router()
 
@@ -16,5 +17,6 @@ const routes = Router()
 routes.post("/categories", new CreateCategoryController().handler)
 routes.get("/categories", new GetAllCategoryController().handler)
 routes.delete("/categories/:id", new DeleteCategoryController().handler)
+routes.put("/categories/:id", new UpdateCategoryController().handler)
 
 export {routes}
